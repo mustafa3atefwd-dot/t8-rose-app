ba888646e6b30615bc365d4a28eca45d118e6c96
mustafa3atef <mustafa3atef.wd@gmail.com>
2026-06-13 15:05:52 +0300

    intialaization

---
+++ src/shared/context/global/providers.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactQueryProvider from './providers/react-query-provider'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+
+export default function providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ReactQueryProvider>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false}/> 
+    </ReactQueryProvider>
+  )
+}
