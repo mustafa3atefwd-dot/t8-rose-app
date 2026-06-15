a73bcc20a0a35a8912521e4adcfc75687e2cb472
radwa <abdelrahmanelsayedtayea@gmail.com>
2026-06-15 19:16:09 +0300

    tailwind-setup

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import '../globals.css';
 import {setRequestLocale} from 'next-intl/server';
 import {hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,17 @@
 import {useTranslations} from 'next-intl';
 
+import { Button } from "@/shared/components/ui/button"
 
 export default function IndexPage(){
   const t = useTranslations('IndexPage');
-  return <h1>{t('title')}</h1>
+  return <div>
+
+    <h1 className='text-red-600'>{t('title')}</h1>
+  <div className="flex flex-wrap items-center gap-2 md:flex-row bg-amber-800">
+      <Button variant="outline">Button</Button>
+      <Button variant="outline" size="icon" aria-label="Submit">
+        <Button variant="destructive">Secondary</Button>
+      </Button>
+    </div>
+  </div>
 } 
