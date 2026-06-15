c631ec5c54176b16bb5701753ec662017efa4e12
radwa <abdelrahmanelsayedtayea@gmail.com>
2026-06-15 19:19:43 +0300

    tailwind setup

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,11 +7,6 @@ export default function IndexPage(){
   return <div>
 
     <h1 className='text-red-600'>{t('title')}</h1>
-  <div className="flex flex-wrap items-center gap-2 md:flex-row bg-amber-800">
-      <Button variant="outline">Button</Button>
-      <Button variant="outline" size="icon" aria-label="Submit">
-        <Button variant="destructive">Secondary</Button>
-      </Button>
-    </div>
+
   </div>
 } 
