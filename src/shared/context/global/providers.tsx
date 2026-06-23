3a0f1413c69efca86de498d8bc9e193467387bd9
mustafa3atef <mustafa3atef.wd@gmail.com>
2026-06-23 16:33:55 +0300

    feat: complete initialization/setup for sprint 1

--- src/shared/context/global/providers.tsx
+++ src/shared/context/global/providers.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import ReactQueryProvider from './providers/react-query-provider'
+import ReactQueryProvider from './providers/react-query.provider'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { NextIntlClientProvider } from 'next-intl'
 
 
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ReactQueryProvider>
-      {children}
       <ReactQueryDevtools initialIsOpen={false}/> 
+      <NextIntlClientProvider>
+          {children}
+      </NextIntlClientProvider>
     </ReactQueryProvider>
   )
 }
