b60c9410f024804f2ad924c626fb20c81f4b3a1b
Ahmed77ehab <ahmedehab30400@gmail.com>
2026-06-25 15:25:14 +0300

    updat

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,19 @@
 import { useTranslations } from 'next-intl';
 import Tests from './components/test';
+import { ThemeToggle } from '@/features/components/toggle-icon';
 
 
 
 export default function IndexPage(){
-  const t = useTranslations();
+  // const t = useTranslations();
   return (
     <div>
 
-    <h1 className='text-red-600'>{t('title')}</h1>
+    {/* <h1 className='text-red-600'>{t('title')}</h1> */}
     <Tests/>
+    <ThemeToggle/>
+    
+    <h2 className='border '>sdfsdf</h2>
 
   </div>
   )
