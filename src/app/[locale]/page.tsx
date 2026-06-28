e57097ef2dd6fb15a5f507b21fbd207c4d3ebd2f
Ahmed77ehab <ahmedehab30400@gmail.com>
2026-06-28 17:29:13 +0300

    clean up eomments, typos

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,9 @@
-import { useTranslations } from 'next-intl';
-import Tests from './components/test';
-import { ThemeToggle } from '@/features/components/toggle-icon';
-
 
 
 export default function IndexPage(){
-  // const t = useTranslations();
   return (
     <div>
-
-    {/* <h1 className='text-red-600'>{t('title')}</h1> */}
-    <Tests/>
-    <ThemeToggle/>
-    
-    <h2 className='border '>sdfsdf</h2>
-
+    <h1>df</h1>
   </div>
   )
 }
--- src/features/components/toggle-icon.tsx
+++ src/features/components/toggle-icon.tsx
@@ -5,44 +5,25 @@ import { useTheme } from "next-themes";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
-  
-  // CAUSE: We need a state to track if the component has arrived in the browser
   const [mounted, setMounted] = useState(false);
 
-  // EFFECT: This only runs ONCE the component is safely inside the browser
   useEffect(() => {
     setMounted(true); 
   }, []);
 
-  // CAUSE/EFFECT: If we aren't mounted yet, render a blank placeholder.
-  // This prevents the Server HTML from mismatching the Browser HTML.
+
   if (!mounted) {
     return <div className="w-9 h-9 items-center justify-center d-flex" />; 
   }
 
-  // Once mounted is true, it is 100% safe to render the real UI
   return (
     <button 
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="p-2 border rounded-md"
     >
-      {theme === "dark" ? "🌙 Dark" : "☀️ Light"}
+      {theme === "dark" ? "☀️ Dark" : "🌙 Light"}
     </button>
   );
 }
 
 
-// import { useTheme } from "next-themes";
-
-// export function ToggleIcon() {
-//   const { resolvedTheme, setTheme } = useTheme();
-
-//   return (
-//     <button
-//       onClick={() =>
-//         setTheme(resolvedTheme === "dark" ? "light" : "dark")
-//       }
-//     >
-//       {resolvedTheme === "dark" ? "🌙" : "☀️"}
-//     </button>
-//   );
