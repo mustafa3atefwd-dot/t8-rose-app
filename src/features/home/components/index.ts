cf0d6929da582090a0ad94b29dfba7c6f457042d
Mahmoudramadan21 <106267261+Mahmoudramadan21@users.noreply.github.com>
2026-07-18 16:27:27 +0300

    feat: add About, Gallery, Testimonials, and Trusted Companies sections; implement responsive design and localization

---
+++ src/features/home/components/index.ts
@@ -0,0 +1,5 @@
+export { default as AboutSection } from './about-section';
+export { default as GallerySection } from './gallery-section';
+export { default as TestimonialItem } from './testimonial-item';
+export { default as TestimonialsSection } from './testimonials-section';
+export { default as TrustedCompaniesSection } from './trusted-companies-section';
