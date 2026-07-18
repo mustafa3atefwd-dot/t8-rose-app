69631f96c73d1e3df87def4a0506c494c4143d9c
Ahmed77ehab <ahmedehab30400@gmail.com>
2026-07-18 22:03:54 +0300

    do unAuthenticated-header with last update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
+import Header from "@/shared/components/header-page";
 import { useTranslations } from "next-intl";
 
 
 export default function Home() {
 
   const t = useTranslations();
-
-  return <div className="flex flex-col items-center justify-center min-h-screen">
-  <h1 className="text-4xl font-bold text-maroon-400">{t("title")}</h1>
-    </div>;
+  
+  return <Header/>
+  // <div className="flex flex-col items-center justify-center min-h-screen">
+  // <h1 className="text-4xl font-bold text-maroon-400">{t("title")}</h1>
+  //   </div>;
 }
---
+++ src/features/auth/components/unauth-login.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { Link } from "@/i18n/navigation"
+import { Loader2, User } from "lucide-react"
+import { useSession } from "next-auth/react"
+import { useTranslations } from "next-intl"
+
+export default function UnAuthenticatedLogin() {
+    const t = useTranslations('homeHeader')
+    const {status } = useSession()
+    if(status === 'unauthenticated'){
+        return <div className="flex items-center h-13 border-r pe-2 ps-2 gap-1.5 whitespace-nowrap">
+        <User />
+        <Link href={'auth/login'}>{t('login')}</Link>
+      </div>
+
+    }
+
+    if(status === 'loading'){
+        return (
+            <div className="flex items-center w-24 h-13 border-r pr-4 pl-4 gap-1.5 justify-center">
+                 <Loader2 className="animate-spin h-5 w-5 text-gray-500" />
+             </div>
+        )
+    }
+    return <>
+            {/* dropdwon menw */}
+
+    </>
+}
