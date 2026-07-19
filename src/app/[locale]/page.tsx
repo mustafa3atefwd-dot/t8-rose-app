882bf4c9396acdf81450d312f4d5ef3b75738f0a
mustafa3atef <mustafa3atef.wd@gmail.com>
2026-07-19 19:50:44 +0300

    refactor(home): address mentor feedback by modularizing hero features and implementing full i18n support

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
-import HomepageHero from '@/shared/components/hero-section';
+
+import HomepageHero from '@/features/home-page/components/hero-main-section';
 import { useTranslations } from 'next-intl';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-maroon-400 text-4xl font-bold">{t('title')}</h1>
+      <HomepageHero/>
     </div>
   );
 }
