376ced6f338154ad736bbb9aa0e29f7d865854ac
Ahmed77ehab <ahmedehab30400@gmail.com>
2026-07-21 02:17:23 +0300

    ui

--- src/app/[locale]/(auth)/products/page.tsx
+++ src/app/[locale]/(auth)/products/page.tsx
@@ -1,3 +1,44 @@
+import Image from "next/image";
+import im from '../../../../assets/images/roseLogo.png'
+import { HeartPlus, ShoppingCart, Star } from "lucide-react";
 export default function Products() {
-    return <h1>Products</h1>
+    return <>
+
+        {/* parent */}
+        <div className="w-238.5 h-full grid grid-cols-3">
+            <div className="w-75.5 h-91 rounded-2xl gap-4">
+                <div className="bg-amber-300 flex flex-col-reverse">
+                    <Image className="w-75.5 h-68 rounded-xl p-2.5" src={im} alt="d" width={200} height={200}/>
+
+                    <div className="flex justify-between ">
+                        <span className="w-7.5 h-7.5 rounded-full pr-1.5 pl-1.5 gap-1.5 bg-white flex justify-center items-center">
+                            <HeartPlus className="w-4.5 h-4.5 text-red-900"/>  
+                        </span>
+
+                        <span className="w-10.5 h-4 rounded-full pt-0.5 pr-2 pb-0.5 pl-2 gap-2.5 bg-zinc-100 flex justify-center items-center">
+                            <p className="font-medium text-xs leading-[100%] tracking-normal text-zinc-700">NEW</p>
+                        </span>
+                    </div>
+                </div>
+
+
+                <div>
+                    <p>Dreamy White Roses Bouquet</p>
+
+                    <div>
+                        <div className="flex">
+                            <div>
+                                <Star />
+                                <div className="flex justify-between">
+                                    <p>250.00 EGP 350.00 EGP</p>
+                                    <ShoppingCart className=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </>
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 import Header from "@/shared/components/header-page";
 import { useTranslations } from "next-intl";
+import Products from "./(auth)/products/page";
 
 
 export default function Home() {
 
-  const t = useTranslations();
+  // const t = useTranslations();
   
-  return <Header/>
+  return <Products/>
   // <div className="flex flex-col items-center justify-center min-h-screen">
   // <h1 className="text-4xl font-bold text-maroon-400">{t("title")}</h1>
   //   </div>;
