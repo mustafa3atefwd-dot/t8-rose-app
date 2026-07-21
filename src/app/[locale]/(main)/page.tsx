80c00551d39505cc82f3eb24de82a1c695a1b912
Eman Saeed <emansaeedhamed22220@gmail.com>
2026-07-21 22:52:51 +0300

    feat: restructure home page components and add new product page

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,6 +1,5 @@
 import BestSelling from '@/features/home/components/best-selling';
 import MostPopular from '@/features/home/components/most-popular';
-import React from 'react';
 
 /** Best Selling and Most Popular fetch server-side; revalidate periodically so rankings don't go stale between deploys. */
 export const revalidate = 60;
