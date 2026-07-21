543b6ecb6994d5f9dc29a0cb16e5d616e92b16cd
Eman Saeed <emansaeedhamed22220@gmail.com>
2026-07-21 17:13:43 +0300

    Merge branch 'feature/register' of https://github.com/mustafa3atefwd-dot/t8-rose-app into feat/header-authenticated

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,5 +7,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-maroon-400 text-4xl font-bold">{t('title')}</h1>
     </div>
+    
+    
   );
 }
