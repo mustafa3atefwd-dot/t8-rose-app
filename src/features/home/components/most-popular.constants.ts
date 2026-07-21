c18f9f9214e7a0e7fd738f7b88d860f08e9c315c
abdosamiir <barakatabdo82@gmail.com>
2026-07-21 11:28:13 +0300

    feat: update MostPopular component to use MOST_POPULAR_OCCASION_TABS_LIMIT for occasion tab capping

--- src/features/home/components/most-popular.constants.ts
+++ src/features/home/components/most-popular.constants.ts
@@ -2,6 +2,7 @@ import { IProduct } from '@/features/products/lib/types/product';
 
 export const MOST_POPULAR_PRODUCTS_LIMIT = 12; // 4 cols x 3 rows
 export const MOST_POPULAR_OCCASIONS_LIMIT = 100;
+export const MOST_POPULAR_OCCASION_TABS_LIMIT = 4;
 
 export interface IOccasionTab {
   id: string;
