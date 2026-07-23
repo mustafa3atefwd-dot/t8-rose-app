2462fd3db3d04a55438ed6a546c495fcd9653b7d
Mahmoudramadan21 <106267261+Mahmoudramadan21@users.noreply.github.com>
2026-07-23 17:52:16 +0300

    refactor: consolidate home component imports and standardize path aliases while removing debug logging

---
+++ src/features/product-details/lib/actions/create-product-review.action.ts
@@ -0,0 +1,32 @@
+import 'server-only';
+
+import { apiRequest } from '@/shared/lib/utils/request.util';
+import { BACKEND_URL } from '@/shared/lib/constants';
+import { IReviewSchema } from '@/features/product-details/lib/types';
+import { ICreateProductReviewResponse } from '@/features/product-details/lib/types';
+import { getNextAuthToken } from '@/shared/lib/utils/auth.util';
+
+/**
+ * Creates a review for a product.
+ */
+export const createProductReviewAction = async (payload: IReviewSchema) => {
+  // Get Auth Token
+  const jwt = await getNextAuthToken();
+  const token = jwt?.accessToken;
+
+  // Construct Request URL
+  const url = `${BACKEND_URL}/reviews`;
+
+  // console.log(jwt);
+  console.log(token);
+  console.log(url);
+
+  return apiRequest<ICreateProductReviewResponse>(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(payload),
+  });
+};
