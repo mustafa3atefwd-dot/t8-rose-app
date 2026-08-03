5c75bc62efc2f81330360e7f8acb5c6d2d6720c8
abdosamiir <barakatabdo82@gmail.com>
2026-08-03 18:52:25 +0300

    feat: enhance internationalization and add product review features

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,16 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'rose-app.elevate-bootcamp.cloud',
-        pathname: '/**'
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
