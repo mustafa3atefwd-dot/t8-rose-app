1cd5d90766a64485d91c6e2830225e0197058c6d
abdosamiir <barakatabdo82@gmail.com>
2026-08-04 18:21:40 +0300

    feat(checkout): add address validation schema

---
+++ src/features/checkout/lib/schemas/address.schema.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { isValidPhoneNumber } from 'libphonenumber-js/max';
+
+export function addressSchema(messages: { required: string; invalidPhone: string }) {
+  return z.object({
+    title: z.string().min(1, messages.required),
+    city: z.string().min(1, messages.required),
+    street: z.string().min(1, messages.required),
+    phone: z
+      .object({
+        phone: z.string().trim(),
+        country: z.string(),
+      })
+      .refine((val) => isValidPhoneNumber(val.phone), {
+        message: messages.invalidPhone,
+      }),
+    latitude: z.string(),
+    longitude: z.string(),
+  });
+}
+
+export type AddressSchema = z.infer<ReturnType<typeof addressSchema>>;
