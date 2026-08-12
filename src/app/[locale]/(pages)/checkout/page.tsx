36bf4138455b045fbde5272fd6483dac83759567
mustafa3atef <mustafa3atef.wd@gmail.com>
2026-08-12 23:28:11 +0300

    feat(checkout): simplify checkout component by removing unused CheckoutView

--- src/app/[locale]/(pages)/checkout/page.tsx
+++ src/app/[locale]/(pages)/checkout/page.tsx
@@ -1,12 +1,7 @@
-import { CheckoutView } from '@/features/checkout/components'
 import CheckoutPage from '@/features/checkout/components/main-checkout-page'
 
 
 export default function checkout() {
-  return (<>
-  
-  <CheckoutPage/>
-    {/* <CheckoutView /> */}
-    </>
+  return (<CheckoutPage/>
   )
 }
